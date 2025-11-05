30a108465b3f7b7d2cbea545787cfccd7fde436b
Murilo Meneguelli <meneguellim7@gmail.com>
2025-11-05 13:01:16 -0300

    .

---
+++ aula11/ex008.js
@@ -0,0 +1,12 @@
+var vel = 18
+
+console.log(`A velocidade do seu carro foi de ${vel}Km/h`)
+
+
+if(vel > 60) { // Condição Simples!
+    console.log(`A velocidade permitida foi exedida, você foi MULTADO!`)
+} 
+
+ console.log(`De olho na estrada e boa viagem, nunca esqueça de dirigir usando cinto de segurança!`)
+
+
