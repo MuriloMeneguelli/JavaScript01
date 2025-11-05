99b4400d43d782558c03c78707efb1e01006694a
Murilo Meneguelli <meneguellim7@gmail.com>
2025-11-05 13:05:03 -0300

    Update ex009.js

---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var país = 'EUA'
+
+if (país == 'BR') {
+    console.log('Seja bem vindo novamente brasileiro, aproveite sua passagem pelo nosso brasil!')
+} else {
+    console.log ('Boas vindas ao brasil estrangeiro, espero que goste do nosso país e suas maravilhas!')
+}
