bc60643786be6ed7ebd39b77dbf09ef52c3a98b3
Murilo Meneguelli <meneguellim7@gmail.com>
2025-11-10 13:26:10 -0300

    .

--- exercícios/script.js
+++ exercícios/script.js
@@ -1,15 +1,20 @@
 function carregar() { 
     var msg = document.getElementById('msg')
-    var img = document.getElementById('imagem')
-    var data = newDate()
+    var img = document.getElementById('foto')
+    var data = new Date()
     //var hora = data.getHours()
-    var hora = 8
+    var hora = 21
     msg.innerHTML = `Agora são ${hora} horas.`
     if (hora >= 0 && hora < 12) {
-        img.scr = 'Manhã.png'
+        img.src = 'Manhã.jpg'
+       
     } else if (hora >= 12 && hora < 18) {
-        img.scr = 'Tarde.png'
+        img.src = 'Tarde.jpg'
+        document.body.style.background ='#222613'
+        
     } else {
-        img.scr = 'Noite.png'
+        img.src = 'Noite.jpg'
+        
     }
 }
+
